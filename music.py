a9591a6d9790b1e6c8a79d1b81731d0fe13dfbf0
tabazu <71821197+tabazu@users.noreply.github.com>
2020-10-15 23:04:45 +0900

    Create music.py

---
+++ music.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+url='https://www.billboard.com/charts/hot-100'
+data = requests.session().get(url, headers=headers)
+def getrank():
+    url='https://www.billboard.com/charts/hot-100'
+    data=requests.session().get(url)
+    soup=BeautifulSoup(data.content, 'html.parser')
+
+    music=soup.select_one('span.chart-element__information')
+    title = music.select_one('span.chart-element__information__song')
+    music_title=title.text.strip()
+    artist = music.select_one('span.chart-element__information__artist')
+    tweet='1 위 '+music_title+artist.text
+    return tweet
