fcda7e3cecd09035d08e5dbb29cb781e1efb626b
tabazu <71821197+tabazu@users.noreply.github.com>
2020-10-15 23:04:41 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+consumer_key=os.environ["CONSUMER_KEY"]
+consumer_secret=os.environ["CONSUMER_SECRET"]
+key=os.environ["KEY"]
+secret=os.environ["SECRET"]
+github_key=os.environ["GITHUB"]
