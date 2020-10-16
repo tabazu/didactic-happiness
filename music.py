50270f5385c5eee8d73627bc7e35e813fadbe370
tabazu <71821197+tabazu@users.noreply.github.com>
2020-10-16 17:27:45 +0900

    Update music.py

--- music.py
+++ music.py
@@ -3,16 +3,15 @@ from bs4 import BeautifulSoup
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-url='https://www.billboard.com/charts/hot-100'
+url='https://www.melon.com/chart/index.htm'
 data = requests.session().get(url, headers=headers)
 def getrank():
-    url='https://www.billboard.com/charts/hot-100'
-    data=requests.session().get(url)
+    url='https://www.melon.com/chart/index.htm'
+    data=requests.session().get(url, headers=headers)
     soup=BeautifulSoup(data.content, 'html.parser')
-
-    music=soup.select_one('span.chart-element__information')
-    title=music.select_one('span.chart-element__information__song')
+    music=soup.select_one('#frm > div > table > tbody > tr:nth-child(1) > td:nth-child(4) > div > div')
+    title = music.select_one('#frm > div > table > tbody > tr:nth-child(1) > td:nth-child(4) > div > div > div.ellipsis.rank01 > span > a')
     music_title=title.text.strip()
-    artist=music.select_one('span.chart-element__information__artist')
-    tweet=music_title+artist.text
+    artist = music.select_one('#frm > div > table > tbody > tr:nth-child(1) > td:nth-child(4) > div > div > div.ellipsis.rank02 > a')
+    tweet='1 . '+music_title+artist.text
     return tweet
